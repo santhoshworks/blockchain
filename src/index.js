3266a94ce1d2a0916cd13c54f5cd7b51f0387381
Santhosh Palanisamy <santhoshp.j4@gmail.com>
2018-06-10 20:43:02 -0400

    commenting materialize for heroku deployment issue

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from 'store';
 import routes from 'routes';
-import 'materialize-css/dist/js/materialize';
-import 'materialize-css/dist/css/materialize.css';
+// import 'materialize-css/dist/js/materialize';
+// import 'materialize-css/dist/css/materialize.css';
 
 render(
   <Provider store={store}>{routes}</Provider>,
